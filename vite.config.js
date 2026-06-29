92e18c8986a047cbb1c077e2d6e2222523963980
paluku-ezechiel <palukuezechiel@gmail.com>
2026-06-29 22:48:49 +0200

    configuration de vite pour utiliser de rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 
 export default defineConfig({
     root: "src",
     build: {
-        outDir: "../dist"
+        outDir: "../dist",
+        rollupOptions:{
+            input: {
+                main: resolve(__dirname, "./src/index.html"),
+                form: resolve(__dirname, "./src/form/form.html")
+            }
+        }
+
     }
 });
