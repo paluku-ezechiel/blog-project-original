918138331167c661b4a3599c5e3a80e231d8faa5
paluku-ezechiel <palukuezechiel@gmail.com>
2026-06-30 13:10:12 +0200

    validation du formulaire et la requete pour ajouter les donnees dans la base

--- src/form/form.js
+++ src/form/form.js
@@ -2,9 +2,48 @@ import "../assets/styles/styles.scss";
 import "./form.scss";
 
 const form = document.querySelector("form");
-form.addEventListener("submit", e => {
+const errorElement = document.querySelector("#error");
+let errors = [];
+
+form.addEventListener("submit", async (e) => {
+  try {
     e.preventDefault();
     const formData = new FormData(form);
     const articles = Object.fromEntries(formData.entries());
-    
-})
+    if(formIsValid(articles)){
+        const json = JSON.stringify(articles);
+        const response = await fetch("https://restapi.fr/api/article", {
+            method: "POST",
+            body: json,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const article = await response.json();
+        console.log(article);
+    }
+  } catch (e) {
+    console.log('erreur : ', e);
+  }
+});
+
+const formIsValid = (articles) => {
+  if (!articles.author || !articles.category || !articles.content) {
+    errors.push("S'il vous plait remplissez tous les champs");
+  } else {
+    errors = [];
+  }
+
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML = `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+    return false;
+  } else {
+    errorElement.innerHTML = "";
+    return true;
+  }
+};
