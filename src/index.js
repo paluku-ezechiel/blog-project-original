019bbefe2d37eca40aad752011b5cf3c8fd847e6
paluku-ezechiel <palukuezechiel@gmail.com>
2026-07-02 10:44:26 +0200

    add menu categories

--- src/index.js
+++ src/index.js
@@ -2,49 +2,60 @@ import "./assets/styles/styles.scss";
 import "./index.scss";
 
 const articleContainer = document.querySelector(".articles-container");
+const categoriesContainerElement = document.querySelector(".categories");
+let filter;
+let articles;
 
-const createArticles = (articles) => {
-  const articlesDOM = articles.map((article) => {
-    const articleDOM = document.createElement("div");
-    articleDOM.classList.add("article");
-    const img = document.createElement("img");
-    img.src = `${article.img}`;
-    img.alt = "profile";
-    const title = document.createElement("h2");
-    title.innerText = `${article.title}`;
-    const articleAuthor = document.createElement("p");
-    articleAuthor.classList.add("article-author");
-    articleAuthor.innerText = `${article.author} ${new Date(
-      article.createdAt
-    ).toLocaleDateString("fr-FR", {
-      weekday: "long",
-      day: "2-digit",
-      month: "long",
-      year: "numeric",
-    })}`;
-    const articleContent = document.createElement("p");
-    articleContent.classList.add("article-content");
-    articleContent.innerHTML = `${article.content}`;
-    const articleActions = document.createElement("div");
-    articleActions.classList.add("article-actions");
-    const buttonDanger = document.createElement("button");
-    buttonDanger.classList.add("btn", "btn-danger");
-    buttonDanger.dataset.id = `${article._id}`;
-    buttonDanger.innerText = "Supprimer";
-    const buttonModifier = document.createElement("button");
-    buttonModifier.classList.add("btn", "btn-primary");
-    buttonModifier.dataset.id = `${article._id}`;
-    buttonModifier.innerText = "Modifier";
-    articleActions.append(buttonModifier, buttonDanger);
-    articleDOM.append(
-      img,
-      title,
-      articleAuthor,
-      articleContent,
-      articleActions
-    );
-    return articleDOM;
-  });
+const createArticles = () => {
+  const articlesDOM = articles
+    .filter((article) => {
+      if (filter) {
+        return article.category === filter;
+      } else {
+        return true;
+      }
+    })
+    .map((article) => {
+      const articleDOM = document.createElement("div");
+      articleDOM.classList.add("article");
+      const img = document.createElement("img");
+      img.src = `${article.img}`;
+      img.alt = "profile";
+      const title = document.createElement("h2");
+      title.innerText = `${article.title}`;
+      const articleAuthor = document.createElement("p");
+      articleAuthor.classList.add("article-author");
+      articleAuthor.innerText = `${article.author} ${new Date(
+        article.createdAt
+      ).toLocaleDateString("fr-FR", {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      })}`;
+      const articleContent = document.createElement("p");
+      articleContent.classList.add("article-content");
+      articleContent.innerHTML = `${article.content}`;
+      const articleActions = document.createElement("div");
+      articleActions.classList.add("article-actions");
+      const buttonDanger = document.createElement("button");
+      buttonDanger.classList.add("btn", "btn-danger");
+      buttonDanger.dataset.id = `${article._id}`;
+      buttonDanger.innerText = "Supprimer";
+      const buttonModifier = document.createElement("button");
+      buttonModifier.classList.add("btn", "btn-primary");
+      buttonModifier.dataset.id = `${article._id}`;
+      buttonModifier.innerText = "Modifier";
+      articleActions.append(buttonModifier, buttonDanger);
+      articleDOM.append(
+        img,
+        title,
+        articleAuthor,
+        articleContent,
+        articleActions
+      );
+      return articleDOM;
+    });
 
   articleContainer.innerHTML = "";
   articleContainer.append(...articlesDOM);
@@ -77,10 +88,51 @@ const createArticles = (articles) => {
   });
 };
 
+const displayMenuCategories = (categoriesArr) => {
+  const liElement = categoriesArr.map((article) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${article[0]} <strong>(${article[1]})</strong>`;
+    li.addEventListener("click", () => {
+      if (filter === article[0]) {
+        filter = null;
+        li.classList.remove("active");
+        createArticles();
+      } else {
+        filter = article[0];
+        liElement.forEach((li) => {
+          li.classList.remove("active");
+        });
+        li.classList.add("active");
+        createArticles();
+      }
+    });
+    return li;
+  });
+  categoriesContainerElement.innerHTML = "";
+  categoriesContainerElement.append(...liElement);
+};
+
+const createMenuCategory = () => {
+  const categories = articles.reduce((acc, value) => {
+    if (acc[value.category]) {
+      acc[value.category]++;
+    } else {
+      acc[value.category] = 1;
+    }
+    return acc;
+  }, {});
+
+  const categoriesArr = Object.keys(categories).map((category) => {
+    return [category, categories[category]];
+  }).sort((c1, c2) => c1[0].localeCompare(c2[0]))
+  displayMenuCategories(categoriesArr);
+};
+
 const fetchArticles = async () => {
   const response = await fetch("https://restapi.fr/api/article");
-  const articles = await response.json();
-  createArticles(articles);
+  articles = await response.json();
+  createArticles();
+  createMenuCategory();
 };
 
 fetchArticles();
